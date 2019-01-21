764dc1152388deb5b27752892b40c5fa75c0f2e5
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2019-01-21 18:04:29 +0000

    Added base url to configuration

--- dev/dev/tests/integration/etc/install-config-mysql.php
+++ dev/dev/tests/integration/etc/install-config-mysql.php
@@ -12,6 +12,7 @@ return [
     'db-prefix' => '',
     'es-hosts' => '127.0.0.1:9201',
     'backend-frontname' => 'backend',
+    'base-url' => 'http://localhost/',
     'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
     'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
     'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
