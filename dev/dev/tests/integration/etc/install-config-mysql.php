82ccfa798e7a72cf76d93c741da9c80e0a494798
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2022-10-19 19:29:14 +0200

    Added detection of environment

--- dev/dev/tests/integration/etc/install-config-mysql.php
+++ dev/dev/tests/integration/etc/install-config-mysql.php
@@ -1,8 +1,24 @@
 <?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
+
+$isDockerEnv = (strpos(getcwd(), '/var/www/projects/') !== false);
+
+if($isDockerEnv) {
+    return [
+        'db-host' => 'db',
+        'db-user' => 'root',
+        'db-password' => 'root',
+        'db-name' => 'magento2_integration_tests',
+        'db-prefix' => '',
+        'es-hosts' => 'elasticsearch:9200',
+        'backend-frontname' => 'backend',
+        'base-url' => 'http://localhost/',
+        'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
+        'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
+        'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
+        'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
+        'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
+    ];
+}
 
 return [
     'db-host' => '127.0.0.1',
