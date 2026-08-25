54e7b490df002ec1f8b7f2c861dce670e16cddd8
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2026-08-25 09:50:07 +0200

    fix: [IPET-3736] Isolate outgoing transport in integration tests

--- shared/dev/tests/integration/etc/config-global.php
+++ shared/dev/tests/integration/etc/config-global.php
@@ -8,4 +8,6 @@ return [
     'customer/password/limit_password_reset_requests_method' => 0,
     'admin/security/admin_account_sharing' => 1,
     'admin/security/limit_password_reset_requests_method' => 0,
+    'email_restrictor/general/is_enabled' => 1,
+    'email_restrictor/general/whitelist' => 'integration-tests.invalid',
 ];
