5841d3bbbb75ed6f92213be5e8890691bea2f37e
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2026-08-25 10:33:38 +0200

    fix: [IPET-3736] Replace email restrictor with SMTP disable in test config

--- shared/dev/tests/integration/etc/config-global.php
+++ shared/dev/tests/integration/etc/config-global.php
@@ -8,6 +8,5 @@ return [
     'customer/password/limit_password_reset_requests_method' => 0,
     'admin/security/admin_account_sharing' => 1,
     'admin/security/limit_password_reset_requests_method' => 0,
-    'email_restrictor/general/is_enabled' => 1,
-    'email_restrictor/general/whitelist' => 'integration-tests.invalid',
+    'system/smtp/disable' => 1,
 ];
